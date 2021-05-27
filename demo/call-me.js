80996898f292281e07dd6a7eec3c9b11b7497760
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2021-05-28 01:38:05 +0300

    Update demo

--- demo/call-me.js
+++ demo/call-me.js
@@ -1,45 +1,38 @@
-const modal = Fitz.createModal({
-    class: {
-        root: 'call-me animate__animated',
-        content: 'animate__animated',
-    },
-    content: document.querySelector('#call-me-template'),
-    hash: '#call-me',
-    style: {
-        // backdropOpacity: 0.9,
-        // backdropBackground: '#000000',
-        // transitionDuration: '250ms',
-    },
-    focus: 'input',
-    aria: {
-        labelledBy: 'header h2',
-        describedBy: 'header p',
-    },
-    animate: {
-        // root: {
-        //     show: 'animate__fadeIn',
-        //     hide: 'animate__fadeOut',
-        // },
-        // content: {
-        //     show: 'animate__bounceInDown',
-        //     hide: 'animate__fadeOutLeft',
-        // },
-        // '[type=submit]': {
-        //     show: 'animate__animated animate__slow animate__bounceInRight',
-        // },
-    }
-})
+(function() {
+    const modal = Fitz.createModal({
+        class: {
+            root: 'call-me',
+            content: 'animate__animated',
+        },
+        content: document.querySelector('#call-me-template'),
+        hash: '#call-me',
+        focus: 'input',
+        aria: {
+            labelledBy: 'header h2',
+            describedBy: 'header p',
+        },
+        animate: {
+            content: {
+                show: 'animate__bounceInDown',
+                hide: 'animate__fadeOutLeft',
+            },
+            '[type=submit]': {
+                show: 'animate__animated animate__slow animate__bounceInUp',
+            },
+        }
+    })
 
-modal.content.addEventListener('submit', (e) => {
-    e.preventDefault()
+    modal.content.addEventListener('submit', (e) => {
+        e.preventDefault()
 
-    e.target.remove()
+        e.target.remove()
 
-    const name = e.target.name.value
-    const phone = e.target.phone.value
+        const name = e.target.name.value
+        const phone = e.target.phone.value
 
-    modal.content.querySelector('header h2').textContent = 'Thank you!'
-    modal.content.querySelector('header p').textContent = `
-        Thank you, ${name}! We will call you at ${phone} as soon as possible.
-    `
-}, { once: true })
+        modal.content.querySelector('header h2').textContent = 'Thank you!'
+        modal.content.querySelector('header p').textContent = `
+            Thank you, ${name}! We will call you at ${phone} as soon as possible.
+        `
+    }, { once: true })
+}())
--- demo/terms-of-use.js
+++ demo/terms-of-use.js
@@ -1,26 +1,10 @@
-Fitz.createModal({
-    class: {
-        root: 'animate__animated',
-        content: 'animate__animated',
-    },
-    content: document.querySelector('#terms-of-use-template'),
-    hash: '#terms-of-use',
-    style: {
-        // backdropOpacity: 0.9,
-        // backdropBackground: '#000000',
-        // transitionDuration: '250ms',
-    },
-    animate: {
-        // root: {
-        //     show: 'animate__fadeIn',
-        //     hide: 'animate__fadeOut',
-        // },
-        // content: {
-        //     show: 'animate__bounceInDown',
-        //     hide: 'animate__fadeOutLeft',
-        // },
-        // '[type=submit]': {
-        //     show: 'animate__animated animate__slow animate__bounceInRight',
-        // },
-    }
-})
+(function() {
+    Fitz.createModal({
+        content: document.querySelector('#terms-of-use-template'),
+        hash: '#terms-of-use',
+        style: {
+            backdropBackground: '#6f42c1',
+            backdropOpacity: '0.9',
+        },
+    })
+}())
